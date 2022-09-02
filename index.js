7dbfe6df35a351f85fb935519659edcfcdb421b7
Yahaya Mubarak <yahayamubarak447@gmail.com>
2022-09-03 00:12:05 +0100

    complete Js, edited CSS

--- index.js
+++ index.js
@@ -13,11 +13,14 @@ let tip;
 let tipResult;
 let totalResult;
 
+// this is the total input
 totalInput.addEventListener('input', () => {
-  total = Number(totalInput.value);
+  total =  Number(totalInput.value);
   console.log(total);
-  // bill.value = calculate(total,people,tip);
+  // bill.value = Number(calculate(total,people,tip));
 });
+
+// this takes the people input
 peopleInput.addEventListener('input', () => {
   if (peopleInput.value < 1) {
     none.textContent = "Can't be zero";
@@ -25,18 +28,29 @@ peopleInput.addEventListener('input', () => {
     none.textContent = '';
     people = Number(peopleInput.value);
     console.log(people); 
-    // bill.value = calculate(total, people, tip);
+    // bill.value = Number(calculate(total, people, tip));
   }
 });
+
+// this is the tip percentages
 tipClick.forEach(tips => tips.addEventListener('click', () => {
   tip = Number(tips.textContent);
-  
   console.log(tip);
-  bill.value = calculate(total, people, tip);
+  bill.value = Number(calculate(total, people, tip));
   tipAmount.textContent = Number(calculateTip(total, tip));
 }));
 
+// this is the custom input
+custom.addEventListener('input', () => {
+  // console.log(custom.value);
+  tip = Number(custom.value);
+  console.log(tip);
+  bill.value = Number(calculate(total, people, tip));
+  tipAmount.textContent = Number(calculateTip(total, tip));
+})
 
+
+// this is the arithmetic operations
 let calculate = (total, people, tip) => 
   ((total * people) + ((tip/100) * total * people));
 
@@ -44,10 +58,10 @@ let calculateTip = (total, tip) => ((tip/100) * total);
 
 
 
-
+// this is the reset button
 reset.addEventListener('click', () => {
-  tipAmount.textContent = '';
+  tipAmount.textContent = '$0.00';
   totalInput.value = '';
   bill.value = '';
-  peopleInput.value = '0';
+  peopleInput.value = '';
 })
